c31132fdd690d9889cb9eec7f89bae61d035021c
Aevann1 <Aevann3@gmail.com>
2021-09-04 17:13:47 +0200

    fd

--- files/__main__.py
+++ files/__main__.py
@@ -10,17 +10,10 @@ from flask_compress import Compress
 from flask_limiter.util import get_ipaddr
 
 from flaskext.markdown import Markdown
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.exc import OperationalError
-from sqlalchemy.orm import sessionmaker, scoped_session, Query as _Query
-from sqlalchemy import *
-from sqlalchemy.pool import QueuePool
-import redis
 import gevent
 import requests
 
 import time
-from redis import ConnectionPool
 
 from os import environ
 from werkzeug.middleware.proxy_fix import ProxyFix
@@ -35,7 +28,6 @@ app.url_map.strict_slashes = False
 app.config["SITE_NAME"]=environ.get("SITE_NAME").strip()
 app.config["COINS_NAME"]=environ.get("COINS_NAME").strip()
 app.config["GUMROAD_LINK"]=environ.get("GUMROAD_LINK", "https://marsey1.gumroad.com/l/tfcvri").strip()
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DATABASE_URL'] = environ.get("DATABASE_CONNECTION_POOL_URL",environ.get("DATABASE_URL"))
 
 app.config['SECRET_KEY'] = environ.get('MASTER_KEY')
@@ -79,17 +71,8 @@ app.config["SPAM_URL_SIMILARITY_THRESHOLD"] = float(environ.get("SPAM_URL_SIMILA
 app.config["COMMENT_SPAM_SIMILAR_THRESHOLD"] = float(environ.get("COMMENT_SPAM_SIMILAR_THRESHOLD", 0.5))
 app.config["COMMENT_SPAM_COUNT_THRESHOLD"] = int(environ.get("COMMENT_SPAM_COUNT_THRESHOLD", 0.5))
 
-app.config["CACHE_REDIS_URL"] = environ.get("REDIS_URL").strip()
 app.config["CACHE_DEFAULT_TIMEOUT"] = 60
 app.config["CACHE_KEY_PREFIX"] = "flask_caching_"
-
-app.config["REDIS_POOL_SIZE"] = 10
-
-redispool=ConnectionPool(
-	max_connections=app.config["REDIS_POOL_SIZE"],
-	host=app.config["CACHE_REDIS_URL"][8:]
-	) if app.config["CACHE_TYPE"]=="redis" else None
-app.config["CACHE_OPTIONS"]={'connection_pool':redispool} if app.config["CACHE_TYPE"]=="redis" else {}
 
 app.config["READ_ONLY"]=bool(int(environ.get("READ_ONLY", "0")))
 app.config["BOT_DISABLE"]=bool(int(environ.get("BOT_DISABLE", False)))
@@ -99,7 +82,6 @@ Markdown(app)
 cache = Cache(app)
 Compress(app)
 
-app.config["RATELIMIT_STORAGE_URL"] = environ.get("REDIS_URL").strip()
 app.config["RATELIMIT_KEY_PREFIX"] = "flask_limiting_"
 app.config["RATELIMIT_ENABLED"] = True
 app.config["RATELIMIT_DEFAULTS_DEDUCT_WHEN"]=lambda:True
@@ -115,53 +97,9 @@ limiter = Limiter(
 	strategy="fixed-window"
 )
 
-_engine=create_engine(
-	app.config['DATABASE_URL'],
-	poolclass=QueuePool,
-	pool_size=int(environ.get("PG_POOL_SIZE",10)),
-	pool_use_lifo=True
-)
-
-def retry(f):
-
-	def wrapper(self, *args, **kwargs):
-		try:
-			return f(self, *args, **kwargs)
-		except:
-			self.session.rollback()
-			return f(self, *args, **kwargs)
-
-	wrapper.__name__=f.__name__
-	return wrapper
-
-
-class RetryingQuery(_Query):
-
-	@retry
-	def all(self):
-		return super().all()
-
-	@retry
-	def count(self):
-		return super().count()
-
-	@retry
-	def first(self):
-		return super().first()
-
-db_session=scoped_session(sessionmaker(bind=_engine, query_cls=RetryingQuery))
 
 Base = declarative_base()
 
-
-#set the shared redis cache for misc stuff
-
-r=redis.Redis(
-	host=app.config["CACHE_REDIS_URL"][8:], 
-	decode_responses=True, 
-	ssl_cert_reqs=None,
-	connection_pool=redispool
-	) if app.config["CACHE_REDIS_URL"] else None
 
 local_ban_cache={}
 
@@ -180,7 +118,6 @@ def get_useragent_ban_response(user_agent_str):
 
 def drop_connection():
 
-	g.db.close()
 	gevent.getcurrent().kill()
 
 
@@ -193,8 +130,6 @@ def before_request():
 
 	if app.config["BOT_DISABLE"] and request.headers.get("X-User-Type")=="Bot":
 		abort(503)
-
-	g.db = db_session()
 
 	g.timestamp = int(time.time())
 
@@ -231,15 +166,6 @@ def before_request():
 
 @app.after_request
 def after_request(response):
-
-	try:
-		g.db.commit()
-		g.db.close()
-	except Exception as e:
-		g.db.rollback()
-		g.db.close()
-		print(e)
-		abort(500)
 
 	response.headers.add("Strict-Transport-Security", "max-age=31536000")
 	response.headers.add("Referrer-Policy", "same-origin")
