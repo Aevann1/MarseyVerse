8a1e4ffb2aa7fe1d0f61c9343ba21de31f3b9ea6
Aevann1 <Aevann3@gmail.com>
2021-09-04 04:14:26 +0200

    fd

--- files/__main__.py
+++ files/__main__.py
@@ -17,6 +17,7 @@ from sqlalchemy import *
 from sqlalchemy.pool import QueuePool
 import redis
 import gevent
+import requests
 
 from redis import ConnectionPool
 
@@ -165,12 +166,6 @@ local_ban_cache={}
 
 UA_BAN_CACHE_TTL = int(environ.get("UA_BAN_CACHE_TTL", 3600))
 
-
-
-# import and bind all routing functions
-from files.routes import *
-import files.helpers.jinja2
-
 @cache.memoize(timeout=UA_BAN_CACHE_TTL)
 def get_useragent_ban_response(user_agent_str):
 	"""
@@ -252,3 +247,39 @@ def after_request(response):
 	response.headers.add("X-Frame-Options", "deny")
 
 	return response
+
+
+@app.template_filter("full_link")
+def full_link(url):
+
+	return f"https://{app.config['SERVER_NAME']}{url}"
+
+@app.template_filter("app_config")
+def app_config(x):
+	return app.config.get(x)
+
+class Post(object):
+      
+    def __init__(self, my_dict):
+          
+        for key in my_dict:
+            setattr(self, key, my_dict[key])
+
+@app.get("/")
+def marseyverse(v):
+	count = 0
+	drama = requests.get("https://rdrama.net/", headers={"Authorization": "sex"}).json()["data"]
+	pcm = requests.get("https://pcmemes.net/", headers={"Authorization": "sex"}).json()["data"]
+	gigachad = requests.get("https://gigachadlife.com/", headers={"Authorization": "sex"}).json()["data"]
+	weebzone = requests.get("https://weebzone.xyz/", headers={"Authorization": "sex"}).json()["data"]
+
+	listing = []
+
+	while count < 100:
+		for site in [drama,pcm,gigachad,weebzone]:
+			try: post = site[count]
+			except: continue
+			listing.append(Post(post))
+		count += 1
+
+	return render_template("marseyverse.html", v=v, listing=listing)
