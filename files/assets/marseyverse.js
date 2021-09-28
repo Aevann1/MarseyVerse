42d4745694ecd08aae8af5d7b9f495f1abbe2de7
Aevann1 <Aevann3@gmail.com>
2021-09-28 22:18:36 +0200

    fds

---
+++ files/assets/marseyverse.js
@@ -0,0 +1,80 @@
+var clipboard = new ClipboardJS('.copy-link');
+clipboard.on('success', function(e) {
+        var myToast = new bootstrap.Toast(document.getElementById('toast-success'));
+        myToast.show();
+        console.log(e);
+});
+clipboard.on('error', function(e) {
+        var myToast = new bootstrap.Toast(document.getElementById('toast-error'));
+        myToast.show();
+        console.log(e);
+});
+
+
+function expandDesktopImage(image) {
+    var linkText = document.getElementById("desktop-expanded-image-link");
+    var imgLink = document.getElementById("desktop-expanded-image-wrap-link");
+
+    var inlineImage = document.getElementById("desktop-expanded-image");
+
+    inlineImage.src = image.replace("100w.webp", "giphy.webp");
+    linkText.href = image;
+    imgLink.href=image;
+
+    linkText.textContent = 'View original';
+};
+
+
+window.onload = function () {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+
+        var topBar = document.getElementById("fixed-bar-mobile");
+
+        var bottomBar = document.getElementById("mobile-bottom-navigation-bar");
+
+        var dropdown = document.getElementById("mobileSortDropdown");
+
+        var navbar = document.getElementById("navbar");
+
+        if (bottomBar != null) {
+            if (prevScrollpos > currentScrollPos && (window.innerHeight + currentScrollPos) < (document.body.offsetHeight - 65)) {
+                bottomBar.style.bottom = "0px";
+            } 
+            else if (currentScrollPos <= 125 && (window.innerHeight + currentScrollPos) < (document.body.offsetHeight - 65)) {
+                bottomBar.style.bottom = "0px";
+            }
+            else if (prevScrollpos > currentScrollPos && (window.innerHeight + currentScrollPos) >= (document.body.offsetHeight - 65)) {
+                bottomBar.style.bottom = "-50px";
+            }
+            else {
+                bottomBar.style.bottom = "-50px";
+            }
+        }
+
+        if (topBar != null && dropdown != null) {
+            if (prevScrollpos > currentScrollPos) {
+                topBar.style.top = "48px";
+                navbar.classList.remove("shadow");
+            } 
+            else if (currentScrollPos <= 125) {
+                topBar.style.top = "48px";
+                navbar.classList.remove("shadow");
+            }
+            else {
+                topBar.style.top = "-48px";
+                dropdown.classList.remove('show');
+                navbar.classList.add("shadow");
+            }
+        }
+        prevScrollpos = currentScrollPos;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function(element){
+        return new bootstrap.Tooltip(element);
+    });
+});
